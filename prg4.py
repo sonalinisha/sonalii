46d0418dfe2d596d5e297c14017b98063cea96fa
sonalinisha <nisham13.nm@gmail.com>
2016-10-14 15:59:31 +0530

    Add files via upload

---
+++ prg4.py
@@ -0,0 +1,14 @@
+b=input("enter no b:")
+a=input("enter value of a:")
+c=input("enter value of c:")
+b=int(b)
+a=int(a)
+c=int(c)
+r1=(-b+(b*b-4*a*c)**0.5)/2*a
+r2=(-b-(b*b-4*a*c)**0.5)/2*a
+print(r1)
+print(r2)
+print("the root1 is:",r1)
+print("the root2 is:",r2)
+input("press enter to close:")
+
